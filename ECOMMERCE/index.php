c527755c8f613b89d28b4387668c78936159c4b7
Stephan Geraci <nzxsterix@gmail.com>
2025-11-12 17:04:22 +0100

    es + style

--- ECOMMERCE/index.php
+++ ECOMMERCE/index.php
@@ -1,10 +1,24 @@
+<?php
+
+    //importo il file db
+
+    require 'db.php';
+
+    //salvo in una variabile $result, i risultati della query
+
+    $result = mysqli_query($conn,'SELECT * FROM contatti'); //query per prendere tutta la tabella dei contatti
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css?v<?= time() ?>">
 </head>
 <body>
 
@@ -26,10 +40,40 @@
                     <th>
                         Email:
                     </th>
+                    <th>
+                        Actions:
+                    </th>
                 </tr>
             </thead>
             <tbody>
 
+                <!--Ciclo WHILE fintanto che ho result, mostrameli in row dedicate-->
+                
+                <?php while($row = mysqli_fetch_assoc( $result)) :  ?>
+                <tr>
+                    <td>
+                        <!--htmlspecialchars serve a convertire i caratteri speciali in entità HTML -->
+                        <?= htmlspecialchars($row['nome']) ?> <!--mostra nome -->
+                    </td>
+
+                    <td>
+                        <?= htmlspecialchars($row['telefono']) ?> <!--mostra telefono -->
+                    </td>
+
+                    <td>    
+                        <?= htmlspecialchars($row['email']) ?> <!--mostra email -->
+                    </td>
+
+                    <td class="actions">
+
+                        <a href="modifica_contatto.php">🖊️</a>
+                        <a href="elimina_contatto.php">🗑️</a>
+                        <a href="ordini.php">📦</a>
+                    
+                    </td>
+                    
+                </tr>
+                <?php endwhile; ?>
             </tbody>
         </table>
     </div>
